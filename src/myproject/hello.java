7c7d3509d8077d691bbcf537831b7675bcec30e2
HARSHBADGUJAR27 <badgujar.harsh1717@gmail.com>
2024-04-06 11:44:04 +0530

    second commit

--- src/myproject/hello.java
+++ src/myproject/hello.java
@@ -2,7 +2,7 @@ package myproject;
 
 public class hello {
 	public static void main(String[] args) {
-		int []marks= {10,20,30,40,50,};
+		int []marks= {10,20,30,40,50,60,70};
 		float max=0;
 		for(int i=0;i<=marks.length;i++)
 		if(i>max)//find highest no
